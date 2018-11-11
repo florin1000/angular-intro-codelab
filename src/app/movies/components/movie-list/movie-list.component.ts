28a5920d670b0d5fa8767ca4ec770e5ffe1bb1c9
Andrei Antal <antal.andrei@icloud.com>
2018-11-11 18:31:08 +0200

    Final code for 5. Rxjs

--- src/app/movies/components/movie-list/movie-list.component.ts
+++ src/app/movies/components/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../model/movie';
 
 @Component({
   selector: 'ngi-movie-list',
@@ -7,20 +8,20 @@ import { MovieService } from '../../services/movie.service';
   styleUrls: ['./movie-list.component.scss']
 })
 export class MovieListComponent implements OnInit {
-  movies;
+  movies: Movie[];
   constructor(private movieService: MovieService) { }
 
   ngOnInit() {
-    this.movies = this.movieService.getMovies();
+    this.movieService.getMovies().subscribe((data) => {
+      this.movies = data;
+    });
   }
 
   handleCommentUpdate(commentPayload) {
-    this.movieService.updateComment(commentPayload.id, commentPayload.newComment)
+    this.movieService.updateComment(commentPayload.id, commentPayload.newComment);
   }
 
   handleMovieDelete(movieId) {
     this.movieService.deleteMovie(movieId);
-    this.movies = this.movieService.getMovies();
   }
-
 }
