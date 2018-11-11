41187e4acb35c2ae419e280d8552fac819d2fb02
Andrei Antal <antal.andrei@icloud.com>
2018-11-11 14:10:57 +0200

    Final code for 1.2 Components - bindings

--- src/app/movies/components/movie-item/movie-item.component.ts
+++ src/app/movies/components/movie-item/movie-item.component.ts
@@ -11,4 +11,21 @@ export class MovieItemComponent {
   genre = 'Action, Adventure, Fantasy';
   plot = 'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.';
   poster = 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg';
+  movieComment = '';
+  commentSaved = false;
+
+  saveComment() {
+    this.commentSaved = !this.commentSaved;
+  }
+
+  wordCount() {
+    if (!this.movieComment || this.movieComment.length === 0) {
+      return 0;
+    } else {
+      return this.movieComment
+        .trim()
+        .replace(/  +/g, ' ')
+        .split(' ').length;
+    }
+  }
 }
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from '../material.module';
@@ -9,6 +10,7 @@ import { MovieItemComponent } from './components/movie-item/movie-item.component
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MaterialModule,
     FlexLayoutModule
   ],
